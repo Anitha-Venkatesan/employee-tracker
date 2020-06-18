d9399b148e45cbb1917f04f47522fe7b24995d4e
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-18 18:33:26 -0500

    inserted roles and department names

--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,24 @@
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES (Anitha,Venkatesan,1,23);
+VALUES ('Anitha','Venkatesan',1,23);
+
+SELECT * FROM employee;
+INSERT INTO department (name) VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+SELECT * FROM department;
+
+INSERT INTO role (title,salary,department_id) VALUES 
+('Sales Lead',100000,1),
+("Sales Person",50000,2),
+("Lead Engineer",100000,3),
+("Software Engineer",80000,4),
+("Accountant",60000,5),
+("Legal Team Lead",120000,6),
+("Lawyer",75000,7);
+
+SELECT * FROM role;
+
+
