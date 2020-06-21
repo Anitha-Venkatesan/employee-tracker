52eccb0420c36d274d18bb2e16d0f0693646e913
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-21 15:24:55 -0500

    added code for mapping the department name and finding the department id

--- employee-tracker-connectionDB.js
+++ employee-tracker-connectionDB.js
@@ -1,5 +1,9 @@
 let inquirer = require("inquirer");
 let mysql = require("mysql");
+const lodash = require('lodash');
+let { viewAllEmployee } = require("./view-all-employee");
+let { viewEmployeeByDepartment } = require("./view-all-employee-by-department");
+const { getAllDepartment } = require("./get-all-department");
 
 // created the connection information for the sql database
 let connection = mysql.createConnection({
@@ -17,10 +21,18 @@ connection.connect(function(err) {
   startQuestions();
 });
 
+function chooseDepartment(departments) {
+  return inquirer.prompt({
+    name: "department",
+    type: "list",
+    message: "Choose Department",
+    choices: lodash.map(departments, 'name')
+  });
+}
+
 // function which prompts the user for what action to do
-function startQuestions() {
-  inquirer
-    .prompt({
+async function startQuestions() {
+  const answer = await inquirer.prompt({
       name: "questionList",
       type: "list",
       message: "What would you like to do for tracking the employee?",
@@ -39,62 +51,54 @@ function startQuestions() {
       "Update Employee Role",
       "Exit" 
     ]
-    })
-    .then(function(answer) {
-      // based on their answer, call that function accordingly
-      if (answer.questionList === "View All Employees") {
-        viewAllEmployee();
-      }
-      else if(answer.questionList === "View All Employees by Department") {
-        viewByDepartment();
-      } 
-      else if(answer.questionList === "View All Employees by Manager") {
-        viewByManager();
-      }
-      else if(answer.questionList === "View All Employees by Role") {
-        viewByRole();
-      }
-      else if(answer.questionList === "Add Employee") {
-        addEmployee();
-      }
-      else if(answer.questionList === "Add Department"){
-         addDepartment(); 
-      }
-      else if(answer.questionList === "Add Role"){
-        addRole(); 
-     }
-     else if(answer.questionList === "Remove Employee") {
-        removeEmployee();
-      }
-      else if(answer.questionList === "Remove Department"){
-         removeDepartment(); 
-      }
-      else if(answer.questionList === "Remove Role"){
-        removeRole(); 
-     }
-      else if(answer.questionList === "Update Employee Role") {
-        updateEmployeeRole();
-      }
-      else if(answer.questionList === "Update Employee Manager") {
-        updateManagerRole();
-      }
-      else if(answer.questionList === "Update Employee Department") {
-        updateDepartmentRole();
-      }
-      else{
-        connection.end();
-      }
     });
+   
+  // based on their answer, call that function accordingly
+  if (answer.questionList === "View All Employees") {
+    viewAllEmployee(connection);
+  }
+  else if(answer.questionList === "View All Employees by Department") {
+    const departments = await getAllDepartment(connection);
+    const response = await chooseDepartment(departments);
+    const selectedDepartment = lodash.find(departments, { name: response.department });
+    viewEmployeeByDepartment(connection, selectedDepartment.id);
+  } 
+  else if(answer.questionList === "View All Employees by Manager") {
+    viewByManager();
+  }
+  else if(answer.questionList === "View All Employees by Role") {
+    viewByRole();
+  }
+  else if(answer.questionList === "Add Employee") {
+    addEmployee();
+  }
+  else if(answer.questionList === "Add Department"){
+      addDepartment(); 
+  }
+  else if(answer.questionList === "Add Role"){
+    addRole(); 
+  }
+  else if(answer.questionList === "Remove Employee") {
+    removeEmployee();
+  }
+  else if(answer.questionList === "Remove Department"){
+      removeDepartment(); 
+  }
+  else if(answer.questionList === "Remove Role"){
+    removeRole(); 
+  }
+  else if(answer.questionList === "Update Employee Role") {
+    updateEmployeeRole();
+  }
+  else if(answer.questionList === "Update Employee Manager") {
+    updateManagerRole();
+  }
+  else if(answer.questionList === "Update Employee Department") {
+    updateDepartmentRole();
+  }
+  else{
+    connection.end();
+  }
 }
 
-// function to handle viewing all the employees on the database employee_tracker_db
-function viewAllEmployee() {
-    connection.query("SELECT * FROM employee", function(error, result) {
-        if (error) throw error;
-    
-        // Log all results of the SELECT statement
-        console.log(result);
-        connection.end();
-    });
-}
-          
+
