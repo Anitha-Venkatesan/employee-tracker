335447d9883d74fb423bdb80ac5455b13ca3f3c5
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-22 16:10:38 -0500

    fixed query issue(gap)

--- js/departments/sum-salary-department.js
+++ js/departments/sum-salary-department.js
@@ -1,6 +1,6 @@
 //function for viewing the total utilized budget of a department
-function sumOfDepartment(connection,departmnet) {
-    connection.query(`select sum(r.salary) as TotalUtilizedBudget, d.id, d.name as departmentfrom 
+function sumOfDepartment(connection) {
+    connection.query(`select sum(r.salary) as TotalUtilizedBudget, d.id, d.name as department from 
     employee e, role r, department d where 
     e.role_id = r.id and 
     r.department_id = d.id
