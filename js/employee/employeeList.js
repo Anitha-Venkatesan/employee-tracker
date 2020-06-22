88fb50fe2be7e540ff3fee6c1a774d3cdf910445
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-22 09:29:14 -0500

    getting name and id for inquirer list

---
+++ js/employee/employeeList.js
@@ -0,0 +1,15 @@
+function allEmployeeList(connection) {
+    return new Promise((resolve, reject) => {
+    connection.query(`SELECT id,first_name,last_name from employee;`,
+    function(error,result) {
+        if (error) {
+            return reject(error);
+        }
+        return resolve(result);
+    });
+  });
+}
+
+  module.exports = {allEmployeeList};
+ 
+
